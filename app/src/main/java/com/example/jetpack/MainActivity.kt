708034c9706eaedbbc89a8dc6035b4cfae7aedeb
deepajais95 <shweta@witbybit.com>
2021-08-08 01:41:08 +0530

    added how to show image in image view

--- app/src/main/java/com/example/jetpack/MainActivity.kt
+++ app/src/main/java/com/example/jetpack/MainActivity.kt
@@ -3,25 +3,11 @@ package com.example.jetpack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.jetpack.datamodels.User
 import com.example.jetpack.ui.theme.JetPackTheme
-import com.example.jetpack.widget.CheckBoxDemo
-import com.example.jetpack.widget.LazyColumnDemo
+import com.example.jetpack.widget.ImageDemo
 
 class MainActivity : ComponentActivity() {
 
@@ -31,7 +17,7 @@ class MainActivity : ComponentActivity() {
             MaterialTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    LazyColumnDemo()
+                  ImageDemo()
                 }
             }
         }
@@ -40,10 +26,11 @@ class MainActivity : ComponentActivity() {
 
 
 
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetPackTheme {
-        LazyColumnDemo()
+
     }
 }
