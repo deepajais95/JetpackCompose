74fa3c8db769f2ea6cda88fd23ebffebd1e8f737
deepajais95 <shweta@witbybit.com>
2021-08-08 00:10:12 +0530

    added recycle view

--- app/src/main/java/com/example/jetpack/MainActivity.kt
+++ app/src/main/java/com/example/jetpack/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -11,10 +13,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.jetpack.datamodels.User
 import com.example.jetpack.ui.theme.JetPackTheme
 import com.example.jetpack.widget.CheckBoxDemo
+import com.example.jetpack.widget.LazyColumnDemo
 
 class MainActivity : ComponentActivity() {
 
@@ -24,7 +31,7 @@ class MainActivity : ComponentActivity() {
             MaterialTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    CheckBoxDemo()
+                    LazyColumnDemo()
                 }
             }
         }
@@ -37,6 +44,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DefaultPreview() {
     JetPackTheme {
-        CheckBoxDemo()
+        LazyColumnDemo()
     }
 }
